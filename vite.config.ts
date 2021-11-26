2438cd7bb601bbfb13e0cc2acd8c90cf965c8c54
“Phenix-G” <eiphenixg@gmail.com>
2021-11-26 17:19:17 +0800

    feat(upgrade): update package and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+      ]
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    }
+  },
+
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    }
+  },
+  server: {
+    fs: {
+      // 移除警告 限制为工作区 root 路径以外的文件的访问
+      strict: false,
+    },
+    port: 8080,
+    // 是否自动在浏览器打开
+    open: true,
+    // 是否开启 https
+    https: false,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_APP_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
+  optimizeDeps: {}
 })
